b8aab238b771c816ee68eb07cc6ece611a283218
guyujung <guyujung@naver.com>
2021-11-22 01:07:09 +0900

    fs모듈 통한 로그인 기능 구현

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -1,13 +1,23 @@
 "use strict";
 
+
+const fs=require("fs").promises;
 class UserStorage{
-    static #users={
-        id : ["guyujung","vega","altair"],
-        psword : ["1234","1234","12345"],
-        name: ["구유정","빵","송준규"],
-    };
+    
+    static #getUserInfo(data,id){
+        const users = JSON.parse(data);
+        const idx=users.id.indexOf(id);
+        const usersKeys=Object.keys(users); //[id,psword,name]
+        const userInfo=usersKeys.reduce((newUser,info)=>{
+            newUser[info]=users[info][idx];
+            return newUser;
+            },{});
+        return userInfo;
+    }
+
+
     static getUsers(...fields){
-        const users=this.#users;
+       ///const users=this.#users;
         const newUsers=fields.reduce((newUsers,field)=>{
             if (users.hasOwnProperty(field)){
                 newUsers[field]=users[field];
@@ -17,18 +27,20 @@ class UserStorage{
         return newUsers;
     }
     static getUserInfo(id){
-        const users=this.#users;
-        const idx=users.id.indexOf(id);
-        const usersKeys=Object.keys(users); //[id,psword,name]
-        const userInfo=usersKeys.reduce((newUser,info)=>{
-            newUser[info]=users[info][idx];
-            return newUser;
-        },{});
-        return userInfo;
+        //const users=this.#users;
+        return fs
+        .readFile("./src/databases/users.json")
+        .then((data)=>{
+            return  this.#getUserInfo(data,id);
+        })
+        .catch(console.error);
+
     }
 
+
+
     static save(userInfo){
-        const users=this.#users;
+        //const users=this.#users;
         users.id.push(userInfo.id);
         users.name.push(userInfo.name);
         users.psword.push(userInfo.psword);
